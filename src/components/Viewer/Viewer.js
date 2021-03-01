70f02b7f0a9e9cc2fe96e1f53cc38765839e7eea
Hardik Joshi <hardikj7321@gmail.com>
2021-03-01 16:46:07 +0530

    changed to default

--- src/components/Viewer/Viewer.js
+++ src/components/Viewer/Viewer.js
@@ -32,7 +32,7 @@ const Viewer = () => {
         firstScript.async = true;
         firstScript.type="text/javascript";
         const secondScript = document.createElement('script');
-        secondScript.src = "%PUBLIC_URL%/adobeview.js";
+        secondScript.src = "/adobeview.js";
         secondScript.async = true;
         secondScript.type="text/javascript";
         document.body.appendChild(firstScript);
